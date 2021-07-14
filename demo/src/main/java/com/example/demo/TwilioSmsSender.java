ec36ed303d45678aef11b35ff53109462a788b3d
jeetpatel24 <friend.jeet24@gmail.com>
2021-07-14 13:42:20 +0530

    TwilioSmsSender class

---
+++ demo/src/main/java/com/example/demo/TwilioSmsSender.java
@@ -0,0 +1,42 @@
+package com.example.demo;
+
+import com.twilio.rest.api.v2010.account.Message;
+import com.twilio.rest.api.v2010.account.MessageCreator;
+import com.twilio.type.PhoneNumber;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Service;
+
+@Service
+public class TwilioSmsSender implements SmsSender{
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(TwilioSmsSender.class);
+
+    private final TwilioConfiguration twilioConfiguration;
+
+    public TwilioSmsSender(TwilioConfiguration twilioConfiguration) {
+        this.twilioConfiguration = twilioConfiguration;
+    }
+
+    @Override
+    public void sendSms(SmsRequest smsRequest) {
+        if(isPhoneNumberValid(smsRequest.getPhoneNumber())) {
+            PhoneNumber to = new PhoneNumber(smsRequest.getPhoneNumber());
+            PhoneNumber from = new PhoneNumber(twilioConfiguration.getTrialNumber());
+            String message = smsRequest.getMessage();
+            MessageCreator creator = Message.creator(
+                    to,
+                    from,
+                    message
+            );
+            LOGGER.info("sms sent : " + smsRequest);
+        } else {
+            throw new IllegalArgumentException("Phone number " + smsRequest.getPhoneNumber() + " is invalid");
+        }
+    }
+
+    private boolean isPhoneNumberValid(String phoneNumber) {
+        // TODO : implement phone number validator
+        return true;
+    }
+}
