ce25b953af269240420b9b750e78bd5ea0f46710
jeetpatel24 <friend.jeet24@gmail.com>
2021-07-14 16:21:47 +0530

    complete solution

--- demo/src/main/java/com/example/demo/TwilioController.java
+++ demo/src/main/java/com/example/demo/TwilioController.java
@@ -1,12 +1,18 @@
 package com.example.demo;
 
+import org.slf4j.ILoggerFactory;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("api/v1/sms")
 public class TwilioController {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(TwilioController.class);
 
     private final TwilioService twilioService;
 
@@ -15,7 +21,8 @@ public class TwilioController {
     }
 
     @PostMapping
-    public void sendSms(SmsRequest smsRequest) {
+    public void sendSms(@RequestBody SmsRequest smsRequest) {
+        LOGGER.info("phone number = " + smsRequest.getPhoneNumber());
         twilioService.sendSms(smsRequest);
     }
 }
